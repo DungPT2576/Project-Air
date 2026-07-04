5d0962c1d7c355ca769bba015c2f169a6353fd0c
Phạm Dũng <Dung.PT2414986@sis.hust.edu.vn>
2026-07-04 14:59:28 +0700

    1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ let dataHistory = []; // Bộ nhớ đệm RAM lưu tối đa 50 điểm
 // ==========================================
 // 1. KẾT NỐI SERVER VỚI MQTT BROKER (LUỒNG REAL-TIME)
 // ==========================================
-const mqttClient = mqtt.connect('mqtt://broker.emqx.io:1883');
+const mqttClient = mqtt.connect('mqtt://broker.hivemq.com:1883');
 const myTopic = 'airsense/hust/tiendung_2507'; // Đã đồng bộ Topic mới
 
 mqttClient.on('connect', () => {
